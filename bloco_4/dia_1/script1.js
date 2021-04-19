f5c57ef76f919b3df65cff8a2446441461e8c999
Mateus <mateusbgonzaga@gmail.com>
2021-04-19 12:58:48 -0300

    Exercise 1 from day 1

---
+++ bloco_4/dia_1/script1.js
@@ -0,0 +1,13 @@
+let name = 'Mateus';
+const birthCity = 'João Pessoa';
+let birthYear = 1976;
+console.log(name, birthCity, birthYear);
+console.log('');
+
+birthYear = 2020;
+console.log(birthYear);
+console.log('');
+
+/*birthCity = 'São Paulo';
+console.log(birthCity);*/
+//Assignment to constant variable.
