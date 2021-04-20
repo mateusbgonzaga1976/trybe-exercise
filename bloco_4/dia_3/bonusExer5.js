22b5ad3b7c1fb68ec949598f3d06157ade84f861
Mateus <mateusbgonzaga@gmail.com>
2021-04-20 17:07:45 -0300

    bonus class - block 4 - day 3 - exercise 5

---
+++ bloco_4/dia_3/bonusExer5.js
@@ -0,0 +1,23 @@
+//I had difficult to solution this exercise,
+//but I would like to keep it in my files,
+//so I copied from 'Trybe gabaritos'; 
+
+let n = 7;
+let middle = (n + 1) / 2;
+let controlLeft = middle;
+let controlRight = middle;
+let symbol = '*';
+
+for (let line = 1; line <= middle; line += 1){
+    let outputLine = '';
+    for(let col = 1; col <= n; col += 1){
+        if(col == controlLeft || col == controlRight || line == middle){
+            outputLine += symbol;
+        }else{
+            outputLine += ' ';
+        }
+    }
+    controlLeft -= 1;
+    controlRight += 1;
+    console.log(outputLine);
+}
